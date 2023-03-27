826fac8a2a15f48278e761ca5fd50209be6b85dc
Austin Patton <SCS374Group1@gmail.com>
2023-03-27 16:09:38 -0400

    Initial Commit

---
+++ SCS 141 Homework 1/SCS_141_Homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SCS_141_Homework_1App.swift
+//  SCS 141 Homework 1
+//
+//  Created by Austin Patton on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SCS_141_Homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
